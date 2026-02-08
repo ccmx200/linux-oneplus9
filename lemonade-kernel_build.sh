14d64c6aee5e5f11e365b08a31eccdc70a6db6a9
cuicanmx <arl96075@163.com>
2026-02-08 22:26:45 +0800

    build(lemonade-kernel): 使用本地配置文件构建内核
    修改构建脚本以使用本地的config-qcom-sm8350配置文件，替代原先的config-postmarketos-qcom-sm8350

--- lemonade-kernel_build.sh
+++ lemonade-kernel_build.sh
@@ -3,7 +3,8 @@ set -e  # 遇到错误立即退出
 
 git clone https://github.com/ccmx200/linux.git -b op9-next-modem
 cd linux
-make -j$(nproc) ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- CC=aarch64-linux-gnu-gcc defconfig config-postmarketos-qcom-sm8350
+cp ../config-qcom-sm8350 config-qcom-sm8350
+make -j$(nproc) ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- CC=aarch64-linux-gnu-gcc defconfig config-qcom-sm8350
 make -j$(nproc) ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- CC=aarch64-linux-gnu-gcc 
 make -j$(nproc) ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- CC=aarch64-linux-gnu-gcc bindeb-pkg
 cd ..
